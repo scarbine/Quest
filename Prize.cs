41d43c8b356d96b7e305020e89de0b02908d4690
Sam Carbine <scarbine@me.com>
2021-08-26 10:29:51 -0500

    phase-6 complete

--- Prize.cs
+++ Prize.cs
@@ -1,12 +1,31 @@
-namespace Quest {
+using System;
+
+
+namespace Quest
+{
 
     public class Prize
     {
-        private string _text {get; set;}
+        private string _text { get; set; }
 
         public Prize(string text)
         {
             _text = text;
+        }
+
+        public void ShowPrize(Adventurer adventurer)
+        {
+            if (adventurer.Awesomeness > 0)
+            {
+                for (var i = 0; i < adventurer.Awesomeness; i++)
+                {
+                    Console.WriteLine($"{_text}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your soul is a lost as Dori...");
+            }
         }
     }
 }
